47f2116db9c23d3ada95a4dec383787943e5ab9f
kkumaki12 <k.kumaki12@gmail.com>
2024-07-10 14:01:13 +0900

    userREpositoryの構造体を定義

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,8 +1,34 @@
 package repository
 
-import "echo-rest-api/model"
+import (
+	"echo-rest-api/model"
+
+	"gorm.io/gorm"
+)
 
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
+}
+
+type userRepositroy struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepositroy{db}
+}
+
+func (ur *userRepositroy) GetUserByEmail(user *model.User, email string) error {
+	if err := ur.db.Where("email = ?", email).First(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ur *userRepositroy) CreateUser(user *model.User) error {
+	if err := ur.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
 }
